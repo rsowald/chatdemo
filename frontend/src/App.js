7a5a3899269ff2ab9723c5e277c56324aaeab5b9
rsowald <72467920+rsowald@users.noreply.github.com>
2022-03-01 14:11:28 -0800

    setting up route structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Channel from "./pages/Channel";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Where do I want to start on my chat application?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/channel/:id" component={Channel} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
