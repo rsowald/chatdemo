74e938411eb5f0a1707d96f9c716ba1f2f6f545d
rsowald <72467920+rsowald@users.noreply.github.com>
2022-03-08 17:02:29 -0800

    adding bootstrap cdn

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button } from "react-bootstrap";
 
 function Login() {
     return (
