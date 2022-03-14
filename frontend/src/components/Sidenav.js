9c6c93559b4287556b0953b0add4566347803c5c
rsowald <72467920+rsowald@users.noreply.github.com>
2022-03-14 09:00:57 -0700

    mapping channel buttons from login data

--- frontend/src/components/Sidenav.js
+++ frontend/src/components/Sidenav.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Stack, Button } from "react-bootstrap";
 
-function Sidenav() {
+function Sidenav({ setSelectedChannel, channels }) {
     return (
-        <Nav variant="pills" defaultActiveKey="/myChannel" className="flex-column">
-            <h3>This will be the Sidenav</h3>
-            <Nav.Link eventKey="myChannel">Channel 1</Nav.Link>
-            <Nav.Link eventKey="channel2">Channel 2</Nav.Link>
-            <Nav.Link eventKey="channel3">Channel 3</Nav.Link>
-            <Nav.Link eventKey="channel4">Channel 4</Nav.Link>
-            <Nav.Link eventKey="channel5">Channel 5</Nav.Link>
-            <Nav.Link eventKey="channel6">Channel 6</Nav.Link>
-        </Nav>
+
+        <Stack gap={2} className="mx-auto">
+            {channels.map(c => <Button key={c} variant="outline-secondary" onClick={() => setSelectedChannel(c)}>{c}</Button>)}
+        </Stack>
     );
 };
 
