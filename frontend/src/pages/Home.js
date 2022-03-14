e84c5685e714cec58f211b20715ca9f23d512c3f
rsowald <72467920+rsowald@users.noreply.github.com>
2022-03-14 09:10:56 -0700

    passing channel and chatClient data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,23 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Sidenav from "../components/Sidenav";
 import Channel from "../components/Channel";
+import React, { useState } from 'react';
 
-function Home() {
+function Home({ channels, username, chatClient }) {
+    const [selectedChannel, setSelectedChannel] = useState(channels[0]);
+
     return (
-        <Container className='container-sm'>
-            <Row className=' justify-content-center'>
-                <Col sm={3}>
-                    <Sidenav />
+        <Container className='container-sm mt-5'>
+            <Row className="text-center mt-4">
+                <h1>Welcome to the {selectedChannel} channel, {username}!</h1>
+            </Row>
+            <Row className='justify-content-center'>
+
+                <Col sm={3} className="mt-3" >
+                    <Sidenav setSelectedChannel={setSelectedChannel} channels={channels} />
                 </Col>
-                <Col sm={7}>
-                    <Channel />
+                <Col sm={7} className="mt-3" >
+                    <Channel selectedChannel={selectedChannel} chatClient={chatClient} />
                 </Col>
             </Row>
 
