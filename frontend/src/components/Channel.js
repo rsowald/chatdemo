2c86f1ad2537018a2c5ce10c23e6d246dc97a8c7
rsowald <72467920+rsowald@users.noreply.github.com>
2022-03-16 09:54:41 -0700

    reorganized to fix race conditions

--- frontend/src/components/Channel.js
+++ frontend/src/components/Channel.js
@@ -1,74 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Container, Form, Button } from "react-bootstrap";
 
 function Channel({ selectedChannel, chatClient }) {
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
+    const scrollRef = useRef();
 
-    const getMessages = async () => {
-        const messageHistory = await chatClient.getMessages({
-            channel: selectedChannel
-        });
-
-        if (!messageHistory?.messages) return;
-
-        setMessages(messageHistory.messages);
-    }
     const publishMessage = async (ev) => {
         ev?.preventDefault();
 
         const content = message.trim();
-        if (!content)
+        if (!content) {
             return;
+        }
 
         await chatClient.publish({
             channel: selectedChannel,
             content
         });
         setMessage("");
-    }
+    };
+
     const keyPress = (ev) => {
-
         if (ev.key === 'Enter' && ev.ctrlKey) {
-            publishMessage()
+            publishMessage();
         }
-    }
-
-    const onLoad = async () => {
-        chatClient.on("message", (message) => {
-            setMessages(oldMessages => [...oldMessages, message]);
-        });
-        getMessages();
-        await chatClient.subscribe(selectedChannel);
-    }
-
-    const onUnload = async () => {
-        chatClient.off("message");
-        await chatClient.unsubscribe(selectedChannel);
-    }
+    };
 
     useEffect(() => {
+        const onLoad = async () => {
+            //Initially we don't strictly need the 'off' or 'unsubscribe'
+            //but it's needed when the 'selectedChannel' prop changes and we need to cleanup before switching to the new channel
+            chatClient.off("message");
+            chatClient.on("message", (message) => {
+                setMessages(oldMessages => [...oldMessages, message]);
+            });
+            try {
+                await chatClient.unsubscribe(selectedChannel);
+            } catch (_) { }
+            await chatClient.subscribe(selectedChannel);
+
+            const messageHistory = await chatClient.getMessages({
+                channel: selectedChannel
+            });
+
+            if (messageHistory?.messages) {
+                setMessages(messageHistory.messages.reverse());
+            }
+
+            scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+        };
+
         onLoad();
-        return onUnload;
     }, [chatClient, selectedChannel]);
 
     return (
         <Container>
-            <div>
-                {/* where to put scrollIntoView(false)? */}
-                {messages.reverse().map(m => {
-                    return <div key={m.id} style={{ 'background': 'lightblue' }}>
-                        <p className='text-muted'>At <em>{m.publishedAt.toLocaleString()}</em> {m.member.id} sent:</p>
-                        <p>{m.content}</p>
+            <div style={{ 'height': '425px', 'overflowY': 'auto' }}>
+                {messages.map(m => {
+                    return <div key={m.id} style={{ 'background': 'lightblue', 'borderRadius': '10px', 'padding': '10px', 'margin': '5px 5px 5px 0' }}>
+                        <div className='text-muted' style={{ 'marginBottom': '5px' }}>At <em>{m.publishedAt.toLocaleString()}</em> {m.member.id} sent:</div>
+                        <span>{m.content}</span>
                     </div>
                 })}
+                <div ref={scrollRef}></div>
             </div>
             <Form onSubmit={publishMessage}>
-                <Form.Control as="textarea" placeholder="Message to Channel" value={message} onKeyUp={keyPress} onChange={(e) => setMessage(e.target.value)} />
-                <Button className="mt-3 mx-3" variant="light" type="submit">
-                    Send
-                </Button>
-                <Form.Text muted>Ctrl+Enter to Send</Form.Text>
+                <Form.Control as="textarea" placeholder="Message to channel" value={message} onKeyUp={keyPress} onChange={(e) => setMessage(e.target.value)} />
+                <div className='d-flex align-items-baseline'>
+                    <Button className="mt-3 mx-3" variant="secondary" type="submit">
+                        Send
+                    </Button>
+                    <Form.Text muted>Ctrl+Enter to send</Form.Text>
+                </div>
             </Form>
         </Container>
     );
